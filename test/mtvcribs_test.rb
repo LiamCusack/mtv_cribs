1a2dec5189a25875b0c93f8321ab0d00d71d97e0
Liam Cusack <LCusack8108@gmail.com>
2020-10-20 09:11:38 -0600

    add test 1

---
+++ test/mtvcribs_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/test'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+require 'pry'
+
+class ApartmentTest < Minitest::Test
+  def test_it_exists
+    renter1 = Renter.new("Jessie")
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+
+    assert_instance_of Renter, renter1
+    assert_instance_of Apartment, unit1
+  end
+end
